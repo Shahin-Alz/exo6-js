083c92f4da8569f03fb0da76334406961ca02520
Shahin-Alz <shaalemzadeh@gmail.com>
2022-12-20 10:03:00 +0100

    ajout

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+let nombre = 8.56;
+console.log(Math.round(8.56));
+
+console.log(Math.ceil(8.56));
+
+console.log(Math.floor(8.56));
+
+console.log(Math.trunc(8.56));
+
+console.log(Math.random(1));
+
+console.log(Math.random(1)*100);
+
+console.log( Math.random, Math.round(1)*100);
+
+console.log(8 ** 2);
+
+console.log(Math.sqrt(9));
+
+console.log(Math.max(-2, 1000, 8, 57));
+
+console.log(Math.min(-2, 1000, 8, 57));
+
+
+
+
+
+
+
+
+
